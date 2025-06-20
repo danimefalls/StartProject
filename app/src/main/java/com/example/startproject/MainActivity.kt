89817464b1918868598449059ce128ee1e4a48cf
danimefalls <kondakov.d,study@gmail.com>
2025-06-20 21:21:10 +0300

    5. Создание обработки запроса

--- app/src/main/java/com/example/startproject/MainActivity.kt
+++ app/src/main/java/com/example/startproject/MainActivity.kt
@@ -12,5 +12,22 @@ class MainActivity : AppCompatActivity() {
         val dayNumberInput = findViewById<EditText>(R.id.dayNumberInput)
         val showDayButton = findViewById<Button>(R.id.showDayButton)
         val dayNameText = findViewById<TextView>(R.id.dayNameText)
+        showDayButton.setOnClickListener {
+            val input = dayNumberInput.text.toString()
+            val dayNumber = input.toIntOrNull()
+
+            val dayName = when (dayNumber) {
+                1 -> "Понедельник"
+                2 -> "Вторник"
+                3 -> "Среда"
+                4 -> "Четверг"
+                5 -> "Пятница"
+                6 -> "Суббота"
+                7 -> "Воскресенье"
+                else -> "Неверный номер дня недели"
+            }
+
+            dayNameText.text = dayName
+        }
     }
 }
